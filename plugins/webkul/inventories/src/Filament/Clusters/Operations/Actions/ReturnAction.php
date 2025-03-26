0bd62b51f2dac4c7fc6b964bf91f4adc950cd7bc
Jitendra <jitendra@webkul.in>
2025-03-26 11:34:40 +0530

    Refactor code

--- plugins/webkul/inventories/src/Filament/Clusters/Operations/Actions/ReturnAction.php
+++ plugins/webkul/inventories/src/Filament/Clusters/Operations/Actions/ReturnAction.php
@@ -3,12 +3,11 @@
 namespace Webkul\Inventory\Filament\Clusters\Operations\Actions;
 
 use Filament\Actions\Action;
-use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Webkul\Inventory\Enums;
 use Webkul\Inventory\Filament\Clusters\Operations\Resources\OperationResource;
 use Webkul\Inventory\Models\Operation;
-use Webkul\Support\Package;
+use Webkul\Inventory\Facades\Inventory;
 
 class ReturnAction extends Action
 {
@@ -26,77 +25,12 @@ class ReturnAction extends Action
             ->color('gray')
             ->requiresConfirmation()
             ->action(function (Operation $record, Component $livewire) {
-                $newRecord = $this->processReturn($record);
+                $newRecord = Inventory::returnTransfer($record);
 
                 $livewire->updateForm();
 
                 return redirect()->to(OperationResource::getUrl('edit', ['record' => $newRecord]));
             })
             ->visible(fn () => $this->getRecord()->state == Enums\OperationState::DONE);
-    }
-
-    /**
-     * Process return for the operation.
-     */
-    public function processReturn(Operation $record): Operation
-    {
-        $newOperation = $record->replicate()->fill([
-            'state'                   => Enums\OperationState::DRAFT,
-            'origin'                  => 'Return of '.$record->name,
-            'operation_type_id'       => $record->operationType->returnOperationType?->id ?? $record->operation_type_id,
-            'source_location_id'      => $record->destination_location_id,
-            'destination_location_id' => $record->source_location_id,
-            'user_id'                 => Auth::id(),
-            'creator_id'              => Auth::id(),
-        ]);
-
-        $newOperation->save();
-
-        foreach ($record->moves as $move) {
-            $newMove = $move->replicate()->fill([
-                'operation_id'            => $newOperation->id,
-                'reference'               => $newOperation->name,
-                'state'                   => Enums\MoveState::DRAFT,
-                'is_refund'               => true,
-                'product_qty'             => $move->product_qty,
-                'product_uom_qty'         => $move->product_uom_qty,
-                'source_location_id'      => $move->destination_location_id,
-                'destination_location_id' => $move->source_location_id,
-                'origin_returned_move_id' => $move->id,
-                'operation_type_id'       => $newOperation->operation_type_id,
-            ]);
-
-            $newMove->save();
-        }
-
-        $newOperation->refresh();
-
-        foreach ($newOperation->moves as $move) {
-            OperationResource::updateOrCreateMoveLines($move);
-        }
-
-        OperationResource::updateOperationState($newOperation);
-
-        $record->update(['return_id' => $record->id]);
-
-        if (Package::isPluginInstalled('purchases')) {
-            $newOperation->purchaseOrders()->attach($record->return->purchaseOrders->pluck('id'));
-        }
-
-        $url = OperationResource::getUrl('view', ['record' => $record]);
-
-        $newOperation->addMessage([
-            'body' => "This transfer has been created from <a href=\"{$url}\" target=\"_blank\" class=\"text-primary-600 dark:text-primary-400\">{$record->name}</a>.",
-            'type' => 'comment',
-        ]);
-
-        $url = OperationResource::getUrl('view', ['record' => $newOperation]);
-
-        $record->addMessage([
-            'body' => "The return <a href=\"{$url}\" target=\"_blank\" class=\"text-primary-600 dark:text-primary-400\">{$newOperation->name}</a> has been created.",
-            'type' => 'comment',
-        ]);
-
-        return $newOperation;
     }
 }
