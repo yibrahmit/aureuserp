fa434d6350a0fc05fa72a80a929b13db99e1be19
Jitendra <jitendra@webkul.in>
2025-03-27 17:06:54 +0530

    Fixed in infolist as well

--- plugins/webkul/security/src/Filament/Resources/UserResource.php
+++ plugins/webkul/security/src/Filament/Resources/UserResource.php
@@ -333,6 +333,9 @@ class UserResource extends Resource
                                         Infolists\Components\TextEntry::make('name')
                                             ->icon('heroicon-o-user')
                                             ->placeholder('—')
+                                            ->extraAttributes([
+                                                'style' => 'word-break: break-all;',
+                                            ])
                                             ->label(__('security::filament/resources/user.infolist.sections.general-information.entries.name')),
                                         Infolists\Components\TextEntry::make('email')
                                             ->icon('heroicon-o-envelope')
