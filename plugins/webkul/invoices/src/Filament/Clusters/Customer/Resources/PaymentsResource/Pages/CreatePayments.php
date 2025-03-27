8907dce5a883521f2a7b478e585a3333eebe87f0
Jitendra <jitendra@webkul.in>
2025-03-27 16:19:09 +0530

    Issue #128 fixed

--- plugins/webkul/invoices/src/Filament/Clusters/Customer/Resources/PaymentsResource/Pages/CreatePayments.php
+++ plugins/webkul/invoices/src/Filament/Clusters/Customer/Resources/PaymentsResource/Pages/CreatePayments.php
@@ -8,4 +8,13 @@ use Webkul\Invoice\Filament\Clusters\Customer\Resources\PaymentsResource;
 class CreatePayments extends BaseCreatePayments
 {
     protected static string $resource = PaymentsResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data = parent::mutateFormDataBeforeCreate($data);
+
+        $data['partner_type'] = 'customer';
+
+        return $data;
+    }
 }
