142c9af4e6020755d05baf8dd8f038ca136e37e9
Dinesh Phuyel <86118075+dp-0@users.noreply.github.com>
2025-03-27 22:15:57 +0545

    Fix: Internal Server Error When Adding a Product to a Category

--- plugins/webkul/products/src/Filament/Resources/CategoryResource/Pages/ManageProducts.php
+++ plugins/webkul/products/src/Filament/Resources/CategoryResource/Pages/ManageProducts.php
@@ -8,8 +8,10 @@ use Filament\Resources\Pages\ManageRelatedRecords;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Support\Facades\Auth;
+use Webkul\Product\Enums\ProductType;
 use Webkul\Product\Filament\Resources\CategoryResource;
 use Webkul\Product\Filament\Resources\ProductResource;
+use Webkul\Support\Models\UOM;
 
 class ManageProducts extends ManageRelatedRecords
 {
@@ -37,7 +39,11 @@ class ManageProducts extends ManageRelatedRecords
                     ->label(__('products::filament/resources/category/pages/manage-products.table.header-actions.create.label'))
                     ->icon('heroicon-o-plus-circle')
                     ->fillForm(function (array $arguments): array {
+                        $uom_id = UOM::first()->id;
                         return [
+                            'type' => ProductType::GOODS->value,
+                            'uom_id' => $uom_id,
+                            'uom_po_id' => $uom_id,
                             'category_id' => $this->getOwnerRecord()->id,
                         ];
                     })
